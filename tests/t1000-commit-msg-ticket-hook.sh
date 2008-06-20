60f9f429547fa737d65c21eb7d99f7167534c817
Stephen Haberman <stephen@exigencecorp.com>
2008-06-19 20:32:21 -0500

    Take out the extra space when going to a file.

--- tests/t1000-commit-msg-ticket-hook.sh
+++ tests/t1000-commit-msg-ticket-hook.sh
@@ -8,57 +8,57 @@ test_description='client commit-msg ticket enforcer'
 install_client_hook 'commit-msg' 'commit-msg'
 
 test_expect_success 'rejects with bad message' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	! git commit -m "first"
 '
 
 test_expect_success 'rejects with re:' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	! git commit -m "first re: #3200"
 '
 
 test_expect_success 'rejects with re no space' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	! git commit -m "first re#3200"
 '
 
 test_expect_success 'accepts with re' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	git commit -m "first re #3200"
 '
 
 test_expect_success 'accepts with re on the second line' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
-	echo "line one" > msg
-	echo "line two re #3200" >> msg
+	echo "line one" >msg
+	echo "line two re #3200" >>msg
 	git commit -F msg
 '
 
 test_expect_success 'accepts with RE' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	git commit -m "first RE #3200"
 '
 
 test_expect_success 'accepts with refs' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	git commit -m "first refs #3200"
 '
 
 test_expect_success 'accepts with qa' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	git commit -m "first qa #3200"
 '
 
 test_expect_success 'accepts with no ticket' '
-	echo "$test_name" > file &&
+	echo "$test_name" >file &&
 	git add file &&
 	git commit -m "first no ticket"
 '
