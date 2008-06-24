f9dd57579fb01a64cf2161818e12f05bbb755939
Stephen Haberman <stephen@exigencecorp.com>
2008-06-23 23:31:24 -0500

    Test the fixes for the shims.

--- tests/t2001-server-pre-receive-trac-via-shim.sh
+++ tests/t2001-server-pre-receive-trac-via-shim.sh
@@ -7,7 +7,7 @@ test_description='server pre-receive trac ticket enforcer via shim'
 test_expect_success 'setup' '
 	echo This is a test. >a &&
 	git add a &&
-	git commit -m "a" &&
+	git commit -m "setup" &&
 	git clone ./. server &&
 	rm -fr server/.git/hooks &&
 	git remote add origin ./server &&
@@ -33,6 +33,44 @@ test_expect_success 'accept with re via shim' '
 	echo $test_name >a &&
 	git commit --amend -m "$test_name re #3222" &&
 	git push
+'
+
+test_expect_success 'reject second push line has bad message via shim' '
+	# make a new remote branch
+	git branch topic1 master &&
+	git push origin topic1 &&
+
+	# change master
+	echo $test_name >a &&
+	git commit -a -m "$test_name re #3222"  &&
+
+	# change topic1 with no re
+	git checkout topic1 &&
+	echo "$test_name topic1" >a &&
+	git commit -a -m "$test_name" &&
+	head=$(git rev-parse HEAD) &&
+
+	! git push >push.out 2>push.err &&
+	cat push.err | grep "Commit $head does not reference a ticket"
+'
+
+test_expect_success 'reject first push line has bad message via shim' '
+	# make a new remote branch
+	git branch topic2 master &&
+	git push origin topic2 &&
+
+	# change master first with no re
+	echo $test_name >a &&
+	git commit -a -m "$test_name"  &&
+	head=$(git rev-parse HEAD) &&
+
+	# change topic2 with re
+	git checkout topic2 &&
+	echo "$test_name topic2" >a &&
+	git commit -a -m "$test_name re #3222" &&
+
+	! git push >push.out 2>push.err &&
+	cat push.err | grep "Commit $head does not reference a ticket"
 '
 
 test_done
